3c12877b0e6c03fd6ccbe55147301d92059864ed
wagany <wanderingganymede@protonmail.com>
2023-11-10 14:43:40 +0100

    initial commit

---
+++ lua/pluginlist.lua
@@ -0,0 +1,42 @@
+return {
+
+	{
+		"numToStr/Comment.nvim",
+		config= function()
+			require("Comment").setup()
+		end
+	},
+	{
+		-- "shaunsingh/solarized.nvim",
+		"shaunsingh/nord.nvim",
+		priority = 1000,
+		config= function()
+			vim.cmd("colorscheme nord")
+		end
+	},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies= {
+			"nvim-tree/nvim-web-devicons"
+		},
+		config= function()
+			require("lualine").setup({
+				icons_enabled=true,
+				theme= 'nord',
+			})
+		end,
+	},
+
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies ={
+	-- 		'L3MON4D3/LuaSnip',
+	-- 		'saadparwaiz1/cmp_luasnip',
+	-- 		'rafamadriz/friendly-snippets',
+	-- 		'hrsh7th/cmp-nvim-lsp',
+	-- 	},
+	-- },
+}
