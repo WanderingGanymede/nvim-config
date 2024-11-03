9873a9ac02523c632e198da5f9cf3c3a4002983d
wagany <wanderingganymede@protonmail.com>
2024-11-03 14:58:02 +0100

    reconfigured lua lsp without mason

--- lua/wagany/plugins/lsp-zero.lua
+++ lua/wagany/plugins/lsp-zero.lua
@@ -87,11 +87,46 @@ return
 			gdscript_config['cmd'] = { 'ncat', 'localhost', os.getenv 'GDScript_Port' or '6005' }
 		end
 		require('lspconfig').gdscript.setup(gdscript_config)
+
+		require 'lspconfig'.lua_ls.setup {
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+						return
+					end
+				end
+
+				client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+					runtime = {
+						-- Tell the language server which version of Lua you're using
+						-- (most likely LuaJIT in the case of Neovim)
+						version = 'LuaJIT'
+					},
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME
+							-- Depending on the usage, you might want to add additional paths here.
+							-- "${3rd}/luv/library"
+							-- "${3rd}/busted/library",
+						}
+						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+						-- library = vim.api.nvim_get_runtime_file("", true)
+					}
+				})
+			end,
+			settings = {
+				Lua = {}
+			}
+		}
+
 		require('mason').setup({})
 		require('mason-lspconfig').setup({
 			-- Replace the language servers listed here
 			-- with the ones you want to install
-			ensure_installed = { 'lua_ls', 'rust_analyzer' },
+			ensure_installed = {},
 			handlers = {
 				function(server_name)
 					require('lspconfig')[server_name].setup({})
